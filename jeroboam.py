0eca2ec2a869390ff98bcc4d68a1980ae0f401b0
Stéphane Péchard <stephanepechard@gmail.com>
2014-09-11 15:26:38 +0200

    Clean tree visualisation
    Directories tree now shows only directories containing at least a file or a directory.
    Directories containing only directories are not clickable, a tooltip specifies it is empty.
    
    Debug mode has been removed from bottle.

--- jeroboam.py
+++ jeroboam.py
@@ -4,11 +4,9 @@
 # stdlib
 from logging.handlers import RotatingFileHandler
 import logging
-import mimetypes
 import os
 import subprocess
 import sys
-from threading import Thread
 
 # create virtualenv before importing pipped package
 VENV_PATH = os.path.join(os.getcwd(), 'venv')
@@ -27,7 +25,7 @@ if not os.path.exists(VENV_PIP):
 
 if not VENV_PYTHON == sys.executable:
     subprocess.call([VENV_PYTHON, os.path.realpath(__file__)])
-    sys.exit(0)  # to stop here the wrong environment
+    sys.exit(0)  # to stop here the non-virtual environment
 
 
 ## Now begins the code using virtual environment
@@ -39,6 +37,7 @@ try:
    input = raw_input  # python2
 except NameError:
    pass  # python3
+from bottle import route, run, static_file, view
 import exifread
 from PIL import Image
 # constants
@@ -46,13 +45,14 @@ APP_NAME = 'jeroboam'
 CONFIG_FILE = 'config.ini'
 CACHE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cache')
 THUMBNAIL_SIZE = '200'  # default
-SUPPORTED_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'png']
+SUPPORTED_EXTENSIONS = ['bmp', 'gif', 'jpg', 'jpeg', 'png']
 ROTATION = {
     'Horizontal (normal)': 0, 'Mirrored horizontal': 0,
     'Rotated 180': 180, 'Mirrored vertical': 0,
     'Mirrored horizontal then rotated 90 CCW': 270, 'Rotated 90 CCW': 270,
     'Mirrored horizontal then rotated 90 CW': 90, 'Rotated 90 CW': 90
 }
+
 
 class Jeroboam:
     def __init__(self, logger):
@@ -88,32 +88,33 @@ class Jeroboam:
             self.log.info("Read and update cache.")
         else:
             os.mkdir(CACHE_DIR)
-            self.log.info("Creating cache directories.")
+            self.log.info("Create cache directories.")
 
         size = (int(self.config.get('DEFAULT', 'thumbnail_size')),
                 int(self.config.get('DEFAULT', 'thumbnail_size')))
-                #int(int(self.config.get('DEFAULT', 'thumbnail_size')) * 1.618))
         for subdir, dirs, files in os.walk(self.config.get('DEFAULT', 'directory')):
             # recreate dirs
             length = len(self.config.get('DEFAULT', 'directory')) + 1
             path = subdir[length:]
-            self.tree.append(path)
             cache_dir = os.path.join(CACHE_DIR, path)
             if not os.path.exists(cache_dir):
                 os.mkdir(cache_dir)
-                #self.log.info("Creating cache dir: " + cache_dir)
+                self.log.info("Creating cache dir: " + cache_dir)
 
             # recreate thumbnails
+            nbfiles = 0
             for file_name in files:
-                #Thread(target=self.create_thumbnail, args=(cache_dir, file_name, subdir, size,)).start()
-                self.create_thumbnail(cache_dir, file_name, subdir, size)
-            # show directory only if supported files in it
-            #if nb_files:
-            #    self.tree.append(path)
+                if file_name.split('.')[-1].lower() in SUPPORTED_EXTENSIONS:
+                    #Thread(target=self.create_thumbnail, args=(cache_dir, file_name, subdir, size,)).start()
+                    self.create_thumbnail(cache_dir, file_name, subdir, size)
+                    nbfiles += 1
+
+            # create tree to be shown
+            self.tree.append({'path': path, 'nbdirs': len(dirs), 'nbfiles': nbfiles})
 
     def create_thumbnail(self, cache_dir, file_name, subdir, size):
         cache_file = os.path.join(cache_dir, file_name)
-        if file_name.split('.')[-1].lower() in SUPPORTED_EXTENSIONS and not os.path.exists(cache_file):
+        if not os.path.exists(cache_file):
             # determine orientation angle
             angle = 0
             file_path = os.path.join(subdir, file_name)
@@ -144,8 +145,6 @@ class Jeroboam:
                 self.log.info('Error while processing: ' + file_path)
 
     def run_bottle(self):
-        from bottle import route, run, static_file, view
-
         @route('/imagelightbox.min.js')
         def imagelightbox():
             return static_file('imagelightbox.min.js', root=os.path.dirname(__file__))
@@ -160,16 +159,23 @@ class Jeroboam:
         @route('/:path#.*#')
         @view('theme')
         def index(path):
-            pic_path = os.path.join(CACHE_DIR, path)
-            if os.path.isdir(pic_path):
-                pictures = [os.path.join(path, pic) for pic in os.listdir(pic_path) if os.path.isfile(os.path.join(pic_path, pic))]
-                return dict(tree=self.tree, pictures=sorted(pictures) if pictures else [])
+            #import ipdb;ipdb.set_trace()
+            full_path = os.path.join(CACHE_DIR, path)
+            if os.path.exists(full_path):
+                if os.path.isdir(full_path):
+                    # list pictures files of the given directory
+                    pictures = [os.path.join(path, pic) for pic in os.listdir(full_path) if os.path.isfile(os.path.join(full_path, pic))]
+                    return dict(tree=self.tree,
+                                pictures=sorted(pictures) if pictures else None,
+                                message="This directory is empty.")
+                else:
+                    dir_path = os.path.join(self.config.get('DEFAULT', 'directory'), path)
+                    return static_file(os.path.basename(full_path), root=os.path.dirname(dir_path))
             else:
-                dir_path = os.path.join(self.config.get('DEFAULT', 'directory'), path)
-                return static_file(os.path.basename(pic_path), root=os.path.dirname(dir_path))
+                return dict(tree=self.tree, pictures=None, message="This directory don't even exist...")
 
         subprocess.Popen(['open', 'http://0.0.0.0:8080'])
-        run(host='0.0.0.0', debug=True)
+        run(host='0.0.0.0')
 
 
 def log():
